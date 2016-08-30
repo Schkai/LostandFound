c5e360804dd9f344f2b7ff59162f3a2c8f1a8a68
Alexander Kalus <ketchum.alabastia@web.de>
2016-08-30 19:14:00 +0200

    TabHost improved

--- app/src/main/java/lostandfound/mi/ur/de/lostandfound/MainActivity.java
+++ app/src/main/java/lostandfound/mi/ur/de/lostandfound/MainActivity.java
@@ -1,8 +1,9 @@
 package lostandfound.mi.ur.de.lostandfound;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
+import android.widget.TabHost;
 
 import java.util.ArrayList;
 
@@ -11,16 +12,33 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<LostItem> itemsMissing;
     private ArrayList<LostItem> itemsFound;
     private ItemArrayAdapter adapter ;
+    private TabHost tabHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        initUI();
+        initLists();
+        initTabs();
 
 
     }
-    private void initUI() {
+    private void initTabs() {
+
+        tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("Lost");
+        tabSpec.setContent(R.id.lost);
+        tabSpec.setIndicator("Lost");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("found");
+        tabSpec.setContent(R.id.found);
+        tabSpec.setIndicator("found");
+        tabHost.addTab(tabSpec);
+    }
+
+    private void initLists() {
         ListView missingList = (ListView) findViewById(R.id.missing_list);
         ListView foundList = (ListView) findViewById(R.id.found_list);
 
